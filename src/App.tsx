6142259dfd7c89b60ca9d3a0841a4ed39960548d
W1llbam <williammedstilredbull@gmail.com>
2026-07-22 22:24:49 +0200

    created header, hero and footer components.

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage.tsx'
 import InvitePage from './pages/InvitePage.tsx'
 import './App.css'
+import Header from './components/Header.tsx'
+import Hero from './components/Hero.tsx'
+import Footer from './components/Footer.tsx'
 
 function App() {
   const [count, setCount] = useState(0) // What does this do?
 
   return (
     <>
+    <Header />
+    <Hero />
+    <Footer />
+
+
     <Routes>
       <Route path="/" element={<HomePage />}/>
       <Route path="/invite" element={<InvitePage />}/>
