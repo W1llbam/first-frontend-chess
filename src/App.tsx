d03fcba14720cb2b4bfa3c0d0932a35622fa6fa8
W1llbam <williammedstilredbull@gmail.com>
2026-07-23 21:33:25 +0200

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import './App.css'
 import Header from './components/Header.tsx'
 import Hero from './components/Hero.tsx'
 import Footer from './components/Footer.tsx'
+import { Route, Routes } from "react-router"
+import CreateMatchPage from './pages/CreateMatchPage.tsx'
+
+function HomePage() {
+  return <Hero />
+}
 
 function App() {
   const [count, setCount] = useState(0) // What does this do?
@@ -10,7 +16,11 @@ function App() {
   return (
     <>
     <Header />
-    <Hero />
+
+    <Routes>
+      <Route path="/" element={<HomePage />}/>
+      <Route path="/create-match" element={<CreateMatchPage />}/>
+    </Routes>
     <Footer />
 
     </>
